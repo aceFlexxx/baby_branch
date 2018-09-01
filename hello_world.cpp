5566b919f8194e06a293e3bc26367a9347bcf7d3
aceFlexxx <elijahkorn@gmail.com>
2018-09-01 19:56:25 -0400

    I made my first commit

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+//Name: Elijah Korneffel
+//Date: 09/01/18
+//Description: Simple Hello World Program
+
+#include <iostream>
+#include <string>
+#using namespace std;
+int main()
+{
+   cout << "Hello, World!" << endl;
+   return 0; 
+}
